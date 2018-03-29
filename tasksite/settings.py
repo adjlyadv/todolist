f9a1fe03cc3dad83e63a33938b9ebb64c1b818bc
adjlyadv <994845300@qq.com>
2018-03-29 07:46:44 -0700

    Update settings.py

--- tasksite/settings.py
+++ tasksite/settings.py
@@ -109,7 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'js/dist/'
